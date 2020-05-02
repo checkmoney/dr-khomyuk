1f6114a6870369dea120b7ec8e4bc48c15e780ca
Igor Kamyshev <igor@kamyshev.me>
2020-05-02 10:15:23 +0700

    fix: add cert to pg connect on production

--- src/external/typeOrmProvider.ts
+++ src/external/typeOrmProvider.ts
@@ -1,11 +1,22 @@
 import { Configuration } from '@solid-soda/config';
 import { TypeOrmModuleAsyncOptions } from '@nestjs/typeorm';
-import { join } from 'path';
+import { join, dirname } from 'path';
+import { readFileSync } from 'fs';
 
 import { ConfigModule } from '&app/config.module';
 
 export const typeOrmProvider: TypeOrmModuleAsyncOptions = {
   useFactory: (config: Configuration) => {
+    const certPath = join(
+      dirname(process.cwd()),
+      '.secure',
+      'ca-certificate.txt',
+    );
+
+    const createSslConfig = this.config.isProd()
+      ? () => ({ ca: readFileSync(certPath) })
+      : () => undefined;
+
     return {
       type: 'postgres',
       host: config.getStringOrThrow('DB_HOST'),
@@ -15,6 +26,7 @@ export const typeOrmProvider: TypeOrmModuleAsyncOptions = {
       database: config.getStringOrThrow('DB_NAME'),
       entities: [join(__dirname, '/../**/*.entity.{ts,js}')],
       synchronize: false,
+      ssl: createSslConfig(),
     };
   },
   inject: [Configuration],
